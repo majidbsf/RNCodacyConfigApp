069e15d532638baa3cfd28f50a3d2b0b8d1749b4
majid-yassin <majid.s@bsfdv.com>
2023-04-06 15:50:32 +0400

    feature: small changes

--- src/screens/HomeScreen/index.test.tsx
+++ src/screens/HomeScreen/index.test.tsx
@@ -47,3 +47,40 @@ describe('render home screen', () => {
     expect(isAuthorizedAfter).toBeFalsy()
   })
 })
+
+describe('render home screen', () => {
+  it('test logout user', () => {
+    const { getByTestId: getByTestIdLoginScreen } = render(
+      <MockedNavigator component={LoginScreen} />
+    )
+
+    const userEmail = getByTestIdLoginScreen('input-user-email')
+    const userPassword = getByTestIdLoginScreen('input-user-password')
+    const appButtonLoginScreen = getByTestIdLoginScreen('app-button')
+
+    fireEvent.changeText(userEmail, 'test@mail.com')
+    fireEvent.changeText(userPassword, '12345678')
+    fireEvent.press(appButtonLoginScreen)
+
+    const {
+      result: {
+        current: { isAuthorized: isAuthorizedBefore },
+      },
+    } = renderHook(() => useStore())
+    expect(isAuthorizedBefore).toBeTruthy()
+
+    const { getByTestId: getByTestIdHomeScreen } = render(
+      <MockedNavigator component={HomeScreen} />
+    )
+
+    const appButtonHomeScreen = getByTestIdHomeScreen('app-button')
+    fireEvent.press(appButtonHomeScreen)
+
+    const {
+      result: {
+        current: { isAuthorized: isAuthorizedAfter },
+      },
+    } = renderHook(() => useStore())
+    expect(isAuthorizedAfter).toBeFalsy()
+  })
+})
--- src/screens/LoginScreen/index.test.tsx
+++ src/screens/LoginScreen/index.test.tsx
@@ -64,3 +64,57 @@ describe('render login screen', () => {
     expect(isAuthorizedAfter).toBeTruthy()
   })
 })
+
+describe('render login screen', () => {
+  it('test login screen invalid input', () => {
+    const { getByTestId, getAllByTestId } = render(
+      <MockedNavigator component={LoginScreen} />
+    )
+
+    const userEmail = getByTestId('input-user-email')
+    const userPassword = getByTestId('input-user-password')
+    const appButton = getByTestId('app-button')
+    const fieldContainerBefore = getAllByTestId('field-container')
+
+    const borderBottomColorBefore =
+      fieldContainerBefore[0].props['style']['borderBottomColor']
+    expect(borderBottomColorBefore).toEqual(AppLightTheme.colors.secondary)
+
+    fireEvent.changeText(userEmail, '')
+    fireEvent.changeText(userPassword, '')
+    fireEvent.press(appButton)
+
+    const fieldContainerAfter = getAllByTestId('field-container')
+
+    const borderBottomColorAfter =
+      fieldContainerAfter[0].props['style']['borderBottomColor']
+    expect(borderBottomColorAfter).toEqual(AppLightTheme.colors.danger)
+  })
+
+  it('test authenticate user', () => {
+    const { getByTestId } = render(<MockedNavigator component={LoginScreen} />)
+
+    const userEmail = getByTestId('input-user-email')
+    const userPassword = getByTestId('input-user-password')
+    const appButton = getByTestId('app-button')
+
+    const {
+      result: {
+        current: { isAuthorized: isAuthorizedBefore },
+      },
+    } = renderHook(() => useStore())
+    expect(isAuthorizedBefore).toBeFalsy()
+
+    fireEvent.changeText(userEmail, 'test@mail.com')
+    fireEvent.changeText(userPassword, '12345678')
+    fireEvent.press(appButton)
+
+    const {
+      result: {
+        current: { isAuthorized: isAuthorizedAfter },
+      },
+    } = renderHook(() => useStore())
+    expect(isAuthorizedBefore).toBeFalsy()
+    expect(isAuthorizedAfter).toBeTruthy()
+  })
+})
